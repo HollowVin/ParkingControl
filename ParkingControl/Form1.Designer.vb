6107093982bf135eab354b043af908f392df7d6e
Arastoruz3 <sochoall93@gmail.com>
2018-04-07 13:09:37 -0500

    Tentativa interfaz

--- ParkingControl/Form1.Designer.vb
+++ ParkingControl/Form1.Designer.vb
@@ -22,10 +22,385 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AyudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ParqueaderoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TabParqueadero = New System.Windows.Forms.TabControl()
+        Me.TabIngreso = New System.Windows.Forms.TabPage()
+        Me.TabFactura = New System.Windows.Forms.TabPage()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.EditarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.TabParqueadero.SuspendLayout()
+        Me.TabIngreso.SuspendLayout()
+        Me.TabFactura.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem, Me.AyudaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ParqueaderoToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(71, 24)
+        Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'AyudaToolStripMenuItem
+        '
+        Me.AyudaToolStripMenuItem.Name = "AyudaToolStripMenuItem"
+        Me.AyudaToolStripMenuItem.Size = New System.Drawing.Size(63, 24)
+        Me.AyudaToolStripMenuItem.Text = "Ayuda"
+        '
+        'ParqueaderoToolStripMenuItem
+        '
+        Me.ParqueaderoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EditarToolStripMenuItem})
+        Me.ParqueaderoToolStripMenuItem.Name = "ParqueaderoToolStripMenuItem"
+        Me.ParqueaderoToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.ParqueaderoToolStripMenuItem.Text = "Parqueadero"
+        '
+        'TabParqueadero
+        '
+        Me.TabParqueadero.AccessibleName = ""
+        Me.TabParqueadero.Controls.Add(Me.TabIngreso)
+        Me.TabParqueadero.Controls.Add(Me.TabFactura)
+        Me.TabParqueadero.Location = New System.Drawing.Point(0, 65)
+        Me.TabParqueadero.Name = "TabParqueadero"
+        Me.TabParqueadero.SelectedIndex = 0
+        Me.TabParqueadero.Size = New System.Drawing.Size(800, 387)
+        Me.TabParqueadero.TabIndex = 1
+        '
+        'TabIngreso
+        '
+        Me.TabIngreso.Controls.Add(Me.Label14)
+        Me.TabIngreso.Controls.Add(Me.TextBox1)
+        Me.TabIngreso.Controls.Add(Me.Label4)
+        Me.TabIngreso.Controls.Add(Me.Label2)
+        Me.TabIngreso.Controls.Add(Me.Label1)
+        Me.TabIngreso.Location = New System.Drawing.Point(4, 25)
+        Me.TabIngreso.Name = "TabIngreso"
+        Me.TabIngreso.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabIngreso.Size = New System.Drawing.Size(792, 358)
+        Me.TabIngreso.TabIndex = 0
+        Me.TabIngreso.Text = "Ingreso"
+        Me.TabIngreso.UseVisualStyleBackColor = True
+        '
+        'TabFactura
+        '
+        Me.TabFactura.Controls.Add(Me.Button1)
+        Me.TabFactura.Controls.Add(Me.TextBox9)
+        Me.TabFactura.Controls.Add(Me.TextBox8)
+        Me.TabFactura.Controls.Add(Me.TextBox7)
+        Me.TabFactura.Controls.Add(Me.TextBox6)
+        Me.TabFactura.Controls.Add(Me.TextBox5)
+        Me.TabFactura.Controls.Add(Me.TextBox4)
+        Me.TabFactura.Controls.Add(Me.TextBox3)
+        Me.TabFactura.Controls.Add(Me.TextBox2)
+        Me.TabFactura.Controls.Add(Me.Label13)
+        Me.TabFactura.Controls.Add(Me.Label12)
+        Me.TabFactura.Controls.Add(Me.Label11)
+        Me.TabFactura.Controls.Add(Me.Label10)
+        Me.TabFactura.Controls.Add(Me.Label9)
+        Me.TabFactura.Controls.Add(Me.Label8)
+        Me.TabFactura.Controls.Add(Me.Label7)
+        Me.TabFactura.Controls.Add(Me.Label6)
+        Me.TabFactura.Location = New System.Drawing.Point(4, 25)
+        Me.TabFactura.Name = "TabFactura"
+        Me.TabFactura.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabFactura.Size = New System.Drawing.Size(792, 358)
+        Me.TabFactura.TabIndex = 1
+        Me.TabFactura.Text = "Factura"
+        Me.TabFactura.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(99, 113)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Hora_Ingreso: "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(458, 113)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Hora_Salida:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(182, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(105, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Disponibles: 45"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(99, 33)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Codigo: "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(451, 45)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(97, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Ocupados: 15"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(165, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(137, 22)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(82, 48)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(73, 17)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Nombres: "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(82, 214)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(71, 17)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "Direccion:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(447, 160)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(44, 17)
+        Me.Label8.TabIndex = 2
+        Me.Label8.Text = "Total:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(82, 281)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(68, 17)
+        Me.Label9.TabIndex = 3
+        Me.Label9.Text = "Telefono:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(82, 98)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(73, 17)
+        Me.Label10.TabIndex = 4
+        Me.Label10.Text = "Apellidos: "
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(447, 98)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(68, 17)
+        Me.Label11.TabIndex = 5
+        Me.Label11.Text = "Subtotal: "
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(447, 48)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(57, 17)
+        Me.Label12.TabIndex = 6
+        Me.Label12.Text = "CI/RUC:"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(82, 160)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(55, 17)
+        Me.Label13.TabIndex = 7
+        Me.Label13.Text = "Fecha: "
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(181, 42)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox2.TabIndex = 8
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(541, 157)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox3.TabIndex = 9
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(541, 95)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox4.TabIndex = 10
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(541, 45)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox5.TabIndex = 11
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(181, 278)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox6.TabIndex = 12
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(181, 209)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox7.TabIndex = 13
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(181, 155)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox8.TabIndex = 14
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(181, 93)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox9.TabIndex = 15
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(102, 179)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(51, 17)
+        Me.Label14.TabIndex = 5
+        Me.Label14.Text = "Placa: "
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(450, 274)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 16
+        Me.Button1.Text = "Imprimir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'EditarToolStripMenuItem
+        '
+        Me.EditarToolStripMenuItem.Name = "EditarToolStripMenuItem"
+        Me.EditarToolStripMenuItem.Size = New System.Drawing.Size(181, 26)
+        Me.EditarToolStripMenuItem.Text = "Editar"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TabParqueadero)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.Label3)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.TabParqueadero.ResumeLayout(False)
+        Me.TabIngreso.ResumeLayout(False)
+        Me.TabIngreso.PerformLayout()
+        Me.TabFactura.ResumeLayout(False)
+        Me.TabFactura.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ParqueaderoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AyudaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TabParqueadero As TabControl
+    Friend WithEvents TabIngreso As TabPage
+    Friend WithEvents TabFactura As TabPage
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents EditarToolStripMenuItem As ToolStripMenuItem
 End Class
--- ParkingControl/Form1.vb
+++ ParkingControl/Form1.vb
@@ -1,3 +1,25 @@
 ﻿Public Class Form1
+    Private Sub Ingreso_Click(sender As Object, e As EventArgs) Handles TabIngreso.Click
 
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub ParqueaderoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ParqueaderoToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub EditarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EditarToolStripMenuItem.Click
+
+    End Sub
 End Class
