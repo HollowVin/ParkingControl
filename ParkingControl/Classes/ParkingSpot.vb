e1c8bb28de51bebf77d99ed689f6dd720ff9aedb
HollowVin <corraljuanmiguel@gmail.com>
2018-04-07 15:28:08 -0500

    Added attributes for ParkingSpot class

--- ParkingControl/Classes/ParkingSpot.vb
+++ ParkingControl/Classes/ParkingSpot.vb
@@ -1,3 +1,10 @@
 ﻿Public Class ParkingSpot
-
+    Private Name As String
+    Private Address As String
+    Private Capacity As Integer
+    Private QuarterHourRate As Decimal
+    Private HalfHourRate As Decimal
+    Private HourRate As Decimal
+    Private OpeningHour As DateTime
+    Private ClosingHour As DateTime
 End Class
