329703274530c4ef1d719d63b86b8380f33f872c
HollowVin <corraljuanmiguel@gmail.com>
2018-04-09 13:19:08 -0500

    Removed attributes from Car and made ParkingSpot handle enters and exits of Cars

--- ParkingControlClasses/Car.vb
+++ ParkingControlClasses/Car.vb
@@ -1,18 +1,10 @@
 ﻿Public Class Car
     Public Property CodeVal As String
     Public Property PlateNumber As String
-    Public Property EnterHour As DateTime
-    Public Property ExitHour As DateTime
+    Public Property EnterTime As DateTime
 
     Public Sub New(CodeVal As String, PlateNumber As String)
         Me.CodeVal = CodeVal
         Me.PlateNumber = PlateNumber
-        EnterHour = DateTime.Now
     End Sub
-
-    Public Function Exits() As TimeSpan
-        ExitHour = DateTime.Now
-        Return ExitHour - EnterHour
-        ' (ExitHour - EnterHour).GetMinutes
-    End Function
 End Class
--- ParkingControlClasses/ParkingSpot.vb
+++ ParkingControlClasses/ParkingSpot.vb
@@ -23,4 +23,40 @@
         Me.WeekendsOpenTimes = WeekendsOpenTimes
         CarsEntered = New List(Of Car)
     End Sub
+
+    Public Function CarEnters(Car As Car) As Boolean
+        If CarsEntered.Contains(Car) Then
+            Return False
+        Else
+            Car.EnterTime = DateTime.Now
+            CarsEntered.Add(Car)
+            Return True
+        End If
+    End Function
+
+    Public Function CarExits(Car As Car) As Decimal
+        If CarsEntered.Contains(Car) Then
+            Dim ExitTime As DateTime = DateTime.Now
+            Dim TimeDifference As TimeSpan = ExitTime - Car.EnterTime
+
+            Dim Hours As Integer = TimeDifference.Hours
+            Dim Minutes As Integer = TimeDifference.Minutes
+            Dim QuarterMinutes = Minutes Mod 15
+
+            Dim ChargeAmount As Decimal
+            ChargeAmount = Hours * HourRate
+
+            If QuarterMinutes = 1 Then
+                ChargeAmount = ChargeAmount + QuarterHourRate
+            ElseIf QuarterMinutes = 2 Then
+                ChargeAmount = ChargeAmount + HalfHourRate
+            Else
+                ChargeAmount = ChargeAmount + HourRate
+            End If
+
+            Return ChargeAmount
+        Else
+            Return 0
+        End If
+    End Function
 End Class
