26ef0440a6e9f5c74e3044a444fa43b5d9971320
HollowVin <corraljuanmiguel@gmail.com>
2018-04-09 13:23:12 -0500

    Uncomment lines on ParkingSpot

--- ParkingControlClasses/ParkingSpot.vb
+++ ParkingControlClasses/ParkingSpot.vb
@@ -6,9 +6,9 @@
     Public Property QuarterHourRate As Decimal
     Public Property HalfHourRate As Decimal
     Public Property HourRate As Decimal
-    'Public Property WorkDaysOpenTimes As OpenTimes
-    'Public Property WeekendsOpenTimes As OpenTimes
-    'Public Property CarsEntered As List(Of Car)
+    Public Property WorkDaysOpenTimes As OpenTimes
+    Public Property WeekendsOpenTimes As OpenTimes
+    Public Property CarsEntered As List(Of Car)
 
     Public Sub New(Code As Integer, Name As String, Address As String, Capacity As Integer, QuarterHourRate As Decimal,
             HalfHourRate As Decimal, HourRate As Decimal)
@@ -21,7 +21,7 @@
         Me.HourRate = HourRate
         'Me.WorkDaysOpenTimes = WorkDaysOpenTimes
         'Me.WeekendsOpenTimes = WeekendsOpenTimes
-        'CarsEntered = New List(Of Car)
+        CarsEntered = New List(Of Car)
     End Sub
 
     Public Function CarEnters(Car As Car) As Boolean
