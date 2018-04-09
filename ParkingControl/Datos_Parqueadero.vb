674fa12bc6a3033a32ed4801aa048a41e2249c1e
Arastoruz3 <sochoall93@gmail.com>
2018-04-09 09:58:59 -0500

    Referencia Control Classes

--- ParkingControl/Datos_Parqueadero.vb
+++ ParkingControl/Datos_Parqueadero.vb
@@ -1,4 +1,9 @@
 ﻿Public Class Datos_Parqueadero
+
+    'ParkingControl.ParkingControlClasses obj = New ParkingControl.ParkingControlClasses()'
+
+
+
     Private Sub DSaveButton_Click(sender As Object, e As EventArgs) Handles DSaveButton.Click
         Me.Hide()
         Parqueadero.Show()
@@ -6,5 +11,9 @@
 
     Private Sub Datos_Parqueadero_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         Application.Exit()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
     End Sub
 End Class
