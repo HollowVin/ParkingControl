1941ad0965d3b3f4b6a89273e0d762c048949121
Arastoruz3 <sochoall93@gmail.com>
2018-04-09 10:32:36 -0500

    Cambio Framework de 4.2.1 a 4.6.1 por problemas de compatibilidad con la libreria

---
+++ ClassLibrary1/Factura.vb
@@ -0,0 +1,19 @@
+﻿Public Class Factura
+    Public Property Number As String
+    Public Property CI As String
+    Public Property Name As String
+    Public Property EmissionDate As Date
+    Public Property Address As String
+    Public Property PhoneNumber As String
+    Public Property Total As Decimal
+
+    Public Sub New(Number As String, CI As String, Name As String, Address As String, PhoneNumber As String, Total As Decimal)
+        Me.Number = Number
+        Me.CI = CI
+        Me.Name = Name
+        EmissionDate = Date.Now
+        Me.Address = Address
+        Me.PhoneNumber = PhoneNumber
+        Me.Total = Total
+    End Sub
+End Class
---
+++ ClassLibrary1/ParkingSpot.vb
@@ -0,0 +1,26 @@
+﻿Public Class ParkingSpot
+    Public Property Code As Integer
+    Public Property Name As String
+    Public Property Address As String
+    Public Property Capacity As Integer
+    Public Property QuarterHourRate As Decimal
+    Public Property HalfHourRate As Decimal
+    Public Property HourRate As Decimal
+    Public Property WorkDaysOpenTimes As OpenTimes
+    Public Property WeekendsOpenTimes As OpenTimes
+    Public Property CarsEntered As List(Of Car)
+
+    Public Sub New(Code As Integer, Name As String, Address As String, Capacity As Integer, QuarterHourRate As Decimal,
+            HalfHourRate As Decimal, HourRate As Decimal, WorkDaysOpenTimes As OpenTimes, WeekendsOpenTimes As OpenTimes)
+        Me.Code = Code
+        Me.Name = Name
+        Me.Address = Address
+        Me.Capacity = Capacity
+        Me.QuarterHourRate = QuarterHourRate
+        Me.HalfHourRate = HalfHourRate
+        Me.HourRate = HourRate
+        Me.WorkDaysOpenTimes = WorkDaysOpenTimes
+        Me.WeekendsOpenTimes = WeekendsOpenTimes
+        CarsEntered = New List(Of Car)
+    End Sub
+End Class
