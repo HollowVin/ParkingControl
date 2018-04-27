43dbb6c3f021f7c888ca4b7fdd640c720ec168eb
HollowVin <corraljuanmiguel@gmail.com>
2018-04-26 20:40:45 -0500

    Changed OpenTimes class, and changed the comboboxes for days so Sunday is the first (for DateTime comparison convenience)

--- ParkingControlClasses/OpenTimes.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class OpenTimes
-    Private OpeningHour As DateTime
-    Private ClosingHour As DateTime
-
-    Public Sub New(OpeningHour As DateTime, ClosingHour As DateTime)
-        Me.OpeningHour = OpeningHour
-        Me.ClosingHour = ClosingHour
-    End Sub
-End Class
