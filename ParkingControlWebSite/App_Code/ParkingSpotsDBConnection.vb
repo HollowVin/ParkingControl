f5b8b7d4ea8e9fdf3d495c706b5aa94ecf08ce25
HollowVin <corraljuanmiguel@gmail.com>
2018-05-12 22:29:13 -0500

    Fixed dependency problem on website

--- ParkingControlWebSite/App_Code/ParkingSpotsDBConnection.vb
+++ ParkingControlWebSite/App_Code/ParkingSpotsDBConnection.vb
@@ -23,14 +23,6 @@ Public Class ParkingSpotsDBConnection
         Return Data
     End Function
 
-    Public Function InsertParkingSpot(P As ParkingControlClasses.ParkingSpot) As String
-        Dim Sentence As String = "INSERT INTO par_parkingspots VALUES (, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-        Dim Command As New Data.OleDb.OleDbCommand(Sentence, connection)
-
-        Command.Parameters.AddWithValue("Name", P.Name)
-
-    End Function
-
     Public Function GetID(ByVal id As Integer) As DataSet
 
         Dim sentence As String = "Select par_name, par_capacity, par_quarter_hour_rate, par_half_hour_rate, par_hour_rate, par_open_from_day, par_open_to_day, par_open_from_hour, par_open_to_hour From par_parkingspots where par_id  = ?"
