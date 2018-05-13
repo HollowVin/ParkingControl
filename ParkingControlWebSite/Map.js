cc6bc0b775253108bfc841777780cd05fc878152
Arastoruz3 <sochoall93@gmail.com>
2018-05-13 00:16:02 -0500

    Added style to the map, geolocation and marker to current position

--- ParkingControlWebSite/Map.js
+++ ParkingControlWebSite/Map.js
@@ -1,51 +1,131 @@
-﻿//var mapcode;
-//var diag;
-//function initialize()
-//{
-//    mapcode = new google.maps.geocoder();
-//    var lnt = new google.maps.latlng(-2.897409, -79.004473);
-
-//    var diagchoice =
-//        {
-//            zoom: 14,
-//            center: lnt,
-//            diagid: google.maps.maptypeid.roadmap
-//        };
-
-//    diag = new google.maps.map(document.getelementbyid('map_populate'), diagchoice);
-//}
-//google.maps.event.adddomlistener(window, 'load', initialize);
-
-var map, infoWindow;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map_populate'), {
+﻿var map, infoWindow;
+function initMap()
+{
+    map = new google.maps.Map(document.getElementById('map_populate'),
+    {
         center: { lat: -2.897409, lng: -79.004473 },
-        zoom: 14
-    });
+        zoom: 17,
+        mapTypeId:'roadmap',
+            styles: [
+                { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+                { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+                { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+                {
+                    featureType: 'administrative.locality',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#d59563' }]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#d59563' }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#263c3f' }]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#6b9a76' }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#38414e' }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry.stroke',
+                    stylers: [{ color: '#212a37' }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#9ca5b3' }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#746855' }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{ color: '#1f2835' }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#f3d19c' }]
+                },
+                {
+                    featureType: 'transit',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#2f3948' }]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#d59563' }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#17263c' }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#515c6d' }]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{ color: '#17263c' }]
+                }
+            ]
+        });
+
     infoWindow = new google.maps.InfoWindow;
 
     // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
+    if (navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(function (position)
+        {
+            var pos =
+            {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
 
+            var marker = new google.maps.Marker
+            ({
+                position: pos,
+                map: map
+            });
+
             infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
+            infoWindow.setContent('Usted está aquí.');
             infoWindow.open(map);
             map.setCenter(pos);
-        }, function () {
+        },
+        function ()
+        {
             handleLocationError(true, infoWindow, map.getCenter());
         });
-    } else {
+    }
+    else
+    {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+function handleLocationError(browserHasGeolocation, infoWindow, pos)
+{
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
         'Error: The Geolocation service failed.' :
