be4bfd3fd6c3662e6efc42b2ff5236ec404f9757
HollowVin <corraljuanmiguel@gmail.com>
2018-05-13 21:12:27 -0500

    Added coordinates class so the parking spot details its geographical latitude and longitude

---
+++ ParkingControlClasses/Coordinates.vb
@@ -0,0 +1,9 @@
+﻿Public Class Coordinates
+    Public Property Latitude As Double
+    Public Property Longitude As Double
+
+    Public Sub New(Latitude As Double, Longitude As Double)
+        Me.Latitude = Latitude
+        Me.Longitude = Longitude
+    End Sub
+End Class
