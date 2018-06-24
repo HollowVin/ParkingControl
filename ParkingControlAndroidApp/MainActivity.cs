b28c40ec5aeeb86342c59f4d44b76006ab032e57
HollowVin <corraljuanmiguel@gmail.com>
2018-06-24 17:41:50 -0500

    Android app now displaying values for the parking spot selected by the user

--- ParkingControlAndroidApp/MainActivity.cs
+++ ParkingControlAndroidApp/MainActivity.cs
@@ -3,12 +3,14 @@ using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
 using System.Data;
+using System;
 
 namespace ParkingControlAndroidApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        DataRowCollection Rows;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -17,21 +19,40 @@ namespace ParkingControlAndroidApp
             SetContentView(Resource.Layout.activity_main);
 
             Spinner parkingSpotsSpinner = FindViewById<Spinner>(Resource.Id.spinner1);
+            parkingSpotsSpinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
 
             var ServiceClient = new ParkingSpotsWebReference.ParkingSpotsService();
             DataSet ParkingSpots = ServiceClient.GetAllValues();
-            DataRowCollection Rows = ParkingSpots.Tables[0].Rows;
+            Rows = ParkingSpots.Tables[0].Rows;
             string[] parkingSpotNames = new string[Rows.Count];
 
             for (int i = 0; i < Rows.Count; i++)
             {
                 parkingSpotNames[i] = Rows[i].ItemArray[1].ToString();
-                parkingSpotNames[i] += " - " + Rows[i].ItemArray[2].ToString();
                 parkingSpotNames[i] += " - " + Rows[i].ItemArray[3].ToString();
+                parkingSpotNames[i] += " - " + Rows[i].ItemArray[2].ToString();
             }
 
             ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerDropDownItem, parkingSpotNames);
             parkingSpotsSpinner.Adapter = adapter;
+        }
+
+        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            TextView quarterHourValue = FindViewById<TextView>(Resource.Id.quarterHourValue);
+            quarterHourValue.Text = "$" + Rows[e.Position].ItemArray[4].ToString();
+
+            TextView halfHourValue = FindViewById<TextView>(Resource.Id.halfHourValue);
+            halfHourValue.Text = "$" + Rows[e.Position].ItemArray[5].ToString();
+
+            TextView hourValue = FindViewById<TextView>(Resource.Id.hourValue);
+            hourValue.Text = "$" + Rows[e.Position].ItemArray[6].ToString();
+
+            TextView openDaysValue = FindViewById<TextView>(Resource.Id.openDaysValue);
+            openDaysValue.Text = "De " + Rows[e.Position].ItemArray[7].ToString() + " a " + Rows[e.Position].ItemArray[8].ToString();
+
+            TextView openHoursValue = FindViewById<TextView>(Resource.Id.openHoursValue);
+            openHoursValue.Text = "De " + Rows[e.Position].ItemArray[9].ToString() + " a " + Rows[e.Position].ItemArray[10].ToString();
         }
     }
 }
